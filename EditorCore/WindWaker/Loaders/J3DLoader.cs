8d90a177a5219c3ff3355bed7c51d754cfa479e3
Matt Hoffman <lord.matt.hoffman@gmail.com>
2015-06-26 23:36:59 -0700

    Added some missing bits from the TEX1 refactor commit.

--- EditorCore/WindWaker/Loaders/J3DLoader.cs
+++ EditorCore/WindWaker/Loaders/J3DLoader.cs
@@ -198,38 +198,6 @@ namespace WEditor.WindWaker.Loaders
                 AssignTextureToMeshRecursive(child, mesh, textures, ref curMaterial, materialList, remapIndexList);
         }
 
-        private static List<Texture2D> LoadTEX1FromFile(EndianBinaryReader reader, long chunkStart)
-        {
-            ushort textureCount = reader.ReadUInt16();
-            ushort padding = reader.ReadUInt16(); // Usually 0xFFFF?
-            uint textureHeaderOffset = reader.ReadUInt32(); // textureCount # bti image headers are stored here, relative to chunkStart.
-            uint stringTableOffset = reader.ReadUInt32(); // One filename per texture. relative to chunkStart.
-            List<Texture2D> textureList = new List<Texture2D>();
-
-            // Get all Texture Names
-            reader.BaseStream.Position = chunkStart + stringTableOffset;
-            StringTable stringTable = StringTable.FromStream(reader);
-
-            for (int t = 0; t < textureCount; t++)
-            {
-                // 0x20 is the length of the BinaryTextureImage header which all come in a row, but then the stream gets jumped around while loading the BTI file.
-                reader.BaseStream.Position = chunkStart + textureHeaderOffset + (t * 0x20);
-                BinaryTextureImage texture = new BinaryTextureImage();
-                texture.Load(reader, chunkStart + 0x20, t);
-
-                Texture2D texture2D = new Texture2D(texture.Width, texture.Height);
-                texture2D.PixelData = texture.GetData();
-                textureList.Add(texture2D);
-
-
-                string executionPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-                texture.SaveImageToDisk(executionPath + "/TextureDump/" + string.Format("[{2}]_{0}_{1}.png", stringTable[t], texture.Format, t));
-            }
-
-            return textureList;
-        }
-
-
         private struct ShapeAttribute
         {
             public J3DFileResource.VertexArrayType ArrayType;
@@ -426,7 +394,7 @@ namespace WEditor.WindWaker.Loaders
                             }
                         }
 
-                        // After we write a primitive, write a speciall null-terminator
+                        // After we write a primitive, write a special null-terminator
                         meshVertexData.Indexes.Add(0xFFFF);
                     }
 
