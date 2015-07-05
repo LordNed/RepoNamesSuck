b8279e9c3d425fa5a0b254e2ce79cea3a6b09d76
Matt Hoffman <lord.matt.hoffman@gmail.com>
2015-07-05 13:30:23 -0700

    Added a Color24/Color32 converter for WPF so that we can use the WPF Toolkit color picker dialog. Fixed some various binding/conversion issues.

---
+++ WindEditor/Common/Color24Converter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using WEditor;
+
+namespace WindEditor.UI
+{
+    public class Color24Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+                Color24 rawColor = (Color24)value;
+                return System.Windows.Media.Color.FromRgb(rawColor.R, rawColor.G, rawColor.B);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var wpfColor = (System.Windows.Media.Color)value;
+            return new Color24(wpfColor.R, wpfColor.G, wpfColor.B);
+        }
+    }
+}
---
+++ WindEditor/Common/Color32Converter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using WEditor;
+
+namespace WindEditor.UI
+{
+    public class Color32Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Color32 rawColor = (Color32)value;
+            return System.Windows.Media.Color.FromArgb(rawColor.A, rawColor.R, rawColor.G, rawColor.B);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var wpfColor = (System.Windows.Media.Color)value;
+            return new Color32(wpfColor.R, wpfColor.G, wpfColor.B, wpfColor.A);
+        }
+    }
+}
