a406570a6b9cb0f4634d9b7f93d12584960970bc
Matt Hoffman <lord.matt.hoffman@gmail.com>
2015-07-06 16:23:58 -0700

    Added a point light and arrow class for maps to use.

---
+++ EditorCore/Common/Maps/Arrow.cs
@@ -0,0 +1,11 @@
+﻿using OpenTK;
+
+namespace WEditor.Common.Maps
+{
+    public class Arrow
+    {
+        public Vector3 Position;
+        public Quaternion Rotation;
+        public short Padding;
+    }
+}
---
+++ EditorCore/Common/Maps/PointLight.cs
@@ -0,0 +1,16 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEditor.Common.Maps
+{
+    public class PointLight
+    {
+        public Vector3 Position;
+        public Vector3 Radius;
+        public Color Color;
+    }
+}
