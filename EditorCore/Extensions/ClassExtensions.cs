540a57d0c70d7a1f221eef5dfd3cc42c9d61a4aa
Matt Hoffman <lord.matt.hoffman@gmail.com>
2015-07-12 13:56:54 -0700

    Commented the NaturalSort extension.

--- EditorCore/Extensions/ClassExtensions.cs
+++ EditorCore/Extensions/ClassExtensions.cs
@@ -7,6 +7,15 @@ namespace WEditor
 {
     public static class ClassExtensions
     {
+        /// <summary>
+        /// Orders a given list by Natural sort. Natural sort is what is most commonly expected (as compared to Alphabetical Sort) when sorting, especially
+        /// when dealing with things with numbers on the end. Natural sort will put it as 1, 2,... 9, 10 while Alphabetical sort puts it as 1, 10, 2, ... 9, 99.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items">List of items to sort</param>
+        /// <param name="selector">Which parameter of the item to sort by</param>
+        /// <param name="stringComparer">Override the string comparerator if needed.</param>
+        /// <returns></returns>
         public static IEnumerable<T> OrderByNatural<T>(this IEnumerable<T> items, Func<T, string> selector, StringComparer stringComparer = null)
         {
             var regex = new Regex(@"\d+", RegexOptions.Compiled);
