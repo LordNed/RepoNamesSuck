183a75e063ab338f129128f141e595ec29bf072b
Matt Hoffman <lord.matt.hoffman@gmail.com>
2015-07-13 00:00:16 -0700

    ...and an update to the main program so that it works with the modified return type of the last commit.

--- ArchiveTools/Program.cs
+++ ArchiveTools/Program.cs
@@ -18,8 +18,8 @@ namespace ArchiveTools
                 var outputStream = Yaz0Decoder.Decode(reader);
 
                 var fileStream = File.Create(@"E:\New_Data_Drive\WindwakerModding\root\res\Stage\Abesso\stage_decompressed.arc");
-                outputStream.BaseStream.Seek(0, SeekOrigin.Begin);
-                outputStream.BaseStream.CopyTo(fileStream);
+                outputStream.Seek(0, SeekOrigin.Begin);
+                outputStream.CopyTo(fileStream);
                 fileStream.Close();
             }
         }
