ddcd230788c6c6bc6716ca2ddbe38b83c519bb1e
Matt Hoffman <lord.matt.hoffman@gmail.com>
2015-07-23 21:17:58 -0700

    EditorCore; Enforce normalization of Ray direction and added a ToString overload to the Ray class.

--- EditorCore/Common/Ray.cs
+++ EditorCore/Common/Ray.cs
@@ -10,7 +10,12 @@ namespace WEditor
         public Ray(Vector3 origin, Vector3 direction)
         {
             Origin = origin;
-            Direction = direction;
+            Direction = direction.Normalized();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Ray (origin: {0:F3}, {1:F3}, {2:F3}, dir: {3:F3}, {4:F3}, {5:F3})", Origin.X, Origin.Y, Origin.Z, Direction.X, Direction.Y, Direction.Z);
         }
     }
 }
