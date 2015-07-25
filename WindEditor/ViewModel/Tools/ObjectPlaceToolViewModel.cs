414cb67c9eae40700475a1121e32840ffc683aa3
Matt Hoffman <lord.matt.hoffman@gmail.com>
2015-07-25 11:38:26 -0700

    Wind Editor: Added support for searching all loaded objects in the Place tool mode.

--- WindEditor/ViewModel/Tools/ObjectPlaceToolViewModel.cs
+++ WindEditor/ViewModel/Tools/ObjectPlaceToolViewModel.cs
@@ -5,53 +5,72 @@ using System.Collections.ObjectModel;
 using System.Globalization;
 using System.IO;
 using System.Reflection;
+using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace WindEditor.UI
 {
-    public class ObjectPlaceEntryViewModel
+    public sealed class TabItem
     {
-        public ImageSource DisplayImage { get; set; }
+        public string Header { get; set; }
+        public List<ObjectCategoryEntry> Content { get; set; }
+
+        public TabItem()
+        {
+            Content = new List<ObjectCategoryEntry>();
+        }
+    }
+
+    public sealed class ObjectCategoryEntry
+    {
+        public string FourCC { get; set; }
+        public string Category { get; set; }
+        public string TechnicalName { get; set; }
         public string DisplayName { get; set; }
+        public string[] Keywords { get; set; }
+        public string IconPath { get; set; }
+        public ImageSource DisplayImage { get; set; }
+
+        public ObjectCategoryEntry()
+        {
+            Keywords = new string[0];
+            DisplayImage = null;
+        }
+
+        public override string ToString()
+        {
+            return DisplayName;
+        }
     }
 
     public class ObjectPlaceToolViewModel
     {
-        public sealed class TabItem
-        {
-            public string Header { get; set; }
-            public List<ObjectPlaceEntryViewModel> Content { get; set; }
-
-            public TabItem()
-            {
-                Content = new List<ObjectPlaceEntryViewModel>();
-            }
-        }
-
-        public sealed class ObjectCategoryEntry
-        {
-            public string FourCC;
-            public string Category;
-            public string TechnicalName;
-            public string DisplayName;
-            public string[] Keywords;
-            public string IconPath;
-
-            public override string ToString()
-            {
-                return DisplayName;
-            }
-        }
-
         public ObservableCollection<TabItem> Tabs { get; private set; }
+        public CollectionViewSource FullList { get; private set; }
+
+        public string SearchFilterText
+        {
+            get { return m_searchFilterText; }
+            set
+            {
+                m_searchFilterText = value;
+
+                if (!string.IsNullOrEmpty(m_searchFilterText))
+                    AddFilter();
+
+                FullList.View.Refresh();
+            }
+        }
 
         private readonly MainWindowViewModel m_mainWindow;
+        private string m_searchFilterText;
 
         public ObjectPlaceToolViewModel(MainWindowViewModel mainWindow)
         {
             m_mainWindow = mainWindow;
             Tabs = new ObservableCollection<TabItem>();
+            FullList = new CollectionViewSource();
 
             LoadTemplatesFromDisk();
         }
@@ -75,6 +94,8 @@ namespace WindEditor.UI
                 objByCategory[categoryData[i].Category].Add(categoryData[i]);
             }
 
+            var fullEntryList = new List<ObjectCategoryEntry>();
+
             // Create tabs for each unique category
             foreach(var kvp in objByCategory)
             {
@@ -93,15 +114,56 @@ namespace WindEditor.UI
                         entryIcon.StreamSource = fs;
                         entryIcon.EndInit();
 
-                        ObjectPlaceEntryViewModel newVM = new ObjectPlaceEntryViewModel();
-                        newVM.DisplayName = entry.DisplayName;
-                        newVM.DisplayImage = entryIcon;
-
-                        tab.Content.Add(newVM);
+                        entry.DisplayImage = entryIcon;
+                        tab.Content.Add(entry);
+                        fullEntryList.Add(entry);
                     }
                 }
 
                 Tabs.Add(tab);
+            }
+
+            // Use the flat-list of our entries and assign it as the source of the CollectionViewSource so we can filter it.
+            FullList.Source = fullEntryList;
+        }
+
+        private void AddFilter()
+        {
+            FullList.Filter -= new FilterEventHandler(Filter);
+            FullList.Filter += new FilterEventHandler(Filter);
+        }
+
+        private void Filter(object sender, FilterEventArgs e)
+        {
+            var src = e.Item as ObjectCategoryEntry;
+            e.Accepted = false;
+            if (src == null)
+                return;
+
+            string searchTerm = SearchFilterText.ToLowerInvariant();
+            
+            // See if the keywords array for the object contains the search term
+            for (int i = 0; i < src.Keywords.Length; i++)
+            {
+                if(src.Keywords[i].ToLowerInvariant().Contains(searchTerm))
+                {
+                    e.Accepted = true;
+                    return;
+                }
+            }
+
+            // See if the technical name contains the search term.
+            if(src.TechnicalName.ToLowerInvariant().Contains(searchTerm))
+            {
+                e.Accepted = true;
+                return;
+            }
+
+            // And finally, see if the display name contains the search term.
+            if(src.DisplayName.ToLowerInvariant().Contains(searchTerm))
+            {
+                e.Accepted = true;
+                return;
             }
         }
     }
