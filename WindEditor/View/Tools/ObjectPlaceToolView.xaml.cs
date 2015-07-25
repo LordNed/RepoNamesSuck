78f080d1e78d10116f416e68c0027a320e9caa7b
Matt Hoffman <lord.matt.hoffman@gmail.com>
2015-07-25 10:45:20 -0700

    Wind Editor: Object Place mode now automatically switches between search results and tabbed view when you type in the search box.

--- WindEditor/View/Tools/ObjectPlaceToolView.xaml.cs
+++ WindEditor/View/Tools/ObjectPlaceToolView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using Xceed.Wpf.Toolkit;
 
 namespace WEditor.UI.View
 {
@@ -23,6 +11,22 @@ namespace WEditor.UI.View
         public ObjectPlaceToolView()
         {
             InitializeComponent();
+            searchResults.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void WatermarkTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = (WatermarkTextBox)sender;
+            if(textBox.Text.Length == 0)
+            {
+                searchResults.Visibility = System.Windows.Visibility.Hidden;
+                tabs.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                searchResults.Visibility = System.Windows.Visibility.Visible;
+                tabs.Visibility = System.Windows.Visibility.Hidden;
+            }
         }
     }
 }
