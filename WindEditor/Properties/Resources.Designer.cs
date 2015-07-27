b5a6d232619cfdadf4c8b27a1e64a57a6078fcca
Matt Hoffman <lord.matt.hoffman@gmail.com>
2015-07-26 17:14:05 -0700

    WindEditor: Changed the default namespace from WEditor.UI to WindEditor.UI

--- WindEditor/Properties/Resources.Designer.cs
+++ WindEditor/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WEditor.UI.Properties {
+namespace WindEditor.UI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WEditor.UI.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WEditor.UI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindEditor.UI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- WindEditor/Properties/Settings.Designer.cs
+++ WindEditor/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WEditor.UI.Properties {
+namespace WindEditor.UI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
